0e49bb5cfd4c51e457dfbe31200dbf67783ec452
RealJohnGalt <johngaltfirstrun@gmail.com>
2023-07-05 20:06:19 +0100

    cupid: support HBM (displayconfig)

--- device.mk
+++ device.mk
@@ -224,6 +224,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# HBM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_4630946545580055170.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946545580055170.xml
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
