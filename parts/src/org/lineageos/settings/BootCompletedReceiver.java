205b2b309f868cc9af73c0325a6abdcfde810eb7
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2023-10-01 19:23:18 +0100

    cupid: parts: Introduce displayfeature color service
    This service relays the current AOSP color mode to the xiaomi
    displayfeature HAL, matching stock behaviour. This is necessary to
    avoid colors being reset after turning the screen off, because
    displayfeature sets color mode everytime the screen is turned on
    defaulting to Vivid mode.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -27,6 +27,7 @@ import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.display.ColorService;
 import org.lineageos.settings.doze.PocketService;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
@@ -44,6 +45,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         RefreshUtils.initialize(context);
         ThermalUtils.startService(context);
         PocketService.startService(context);
+        ColorService.startService(context);
 
         // Override HDR types
         final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
