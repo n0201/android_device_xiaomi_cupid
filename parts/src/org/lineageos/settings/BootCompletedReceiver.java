d04de4d26b587ca7a81d8afd79d1699dc1f92f25
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2023-10-28 22:07:34 +0100

    Revert "cupid: parts: Override HDR types for dolby vision"
    This reverts commit 5ab66c5157116d31b97385e6a55c36fcec32c2f1.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,9 +20,6 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
-import android.view.Display.HdrCapabilities;
-import android.view.SurfaceControl;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
@@ -47,10 +44,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         PocketService.startService(context);
         ColorService.startService(context);
 
-        // Override HDR types
-        final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
-        SurfaceControl.overrideHdrTypes(displayToken, new int[]{
-                HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
-                HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
     }
 }
