13037fb30376071032b31ef1c0f8796891c29402
n0201 <87095139+n0201@users.noreply.github.com>
2024-02-01 22:06:04 +0000

    fixed BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,7 @@ DEVICE_MANIFEST_TARO_FILES := \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
-    vendor/evolution/config/device_framework_matrix.xml
+    vendor/derp/config/device_framework_matrix.xml
 
 # Kernel
 # is-board-platform-in-list is used in split files below
@@ -132,8 +132,8 @@ BOARD_VENDOR_RAMDISK_FRAGMENT.dlkm.KERNEL_MODULE_DIRS := top
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
-    bootinfo.fingerprint=$(EVOLUTION_VERSION) \
-    mtdoops.fingerprint=$(EVOLUTION_VERSION)
+    bootinfo.fingerprint=$(DERP_VERSION) \
+    mtdoops.fingerprint=$(DERP_VERSION)
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
